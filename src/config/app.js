ff0f09d1a39a5b9df88021a2989b824646375c30
Francisco Igor <franciscoigor@gmail.com>
2018-04-12 13:18:18 -0700

    Updated app config file and index

--- src/config/app.js
+++ src/config/app.js
@@ -2,5 +2,6 @@ module.exports = {
   title: 'Vuetify Cordova',
   description: 'Vuetify PWA Application Template for Cordova Application Projects',
   url: 'https://fraigo.github.io/vuetify-cordova/www/',
-  icon: 'img/icons/icon.png'
+  icon: 'img/icons/icon.png',
+  iconSize: 256
 }
